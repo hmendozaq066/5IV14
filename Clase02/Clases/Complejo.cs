49bb940b9f6dc1aa45e1b12de6551917cf048df5
HECTOR MANUEL MENDOZA QUIROZ <hmendoza@ipn.mx>
2021-09-03 18:54:20 -0500

    0654 03/09/2021

--- Clase02/Clases/Complejo.cs
+++ Clase02/Clases/Complejo.cs
@@ -26,6 +26,13 @@ namespace Clase02.Clases
         //Este es un constructor porque:
         //1- No devuelve valores
         //2- Se llama igual que la clase
+
+        /// <summary>
+        /// Constructor que solicita 2 parámetros 
+        /// Comentario que usa intellisense
+        /// </summary>
+        /// <param name="A">Parte real del número complejo</param>
+        /// <param name="b">Parte imaginaria del número complejo</param>
         public Complejo(int A, int b)
         {
             //Cuando utilizar this: Si una propiedad se llama igual que una variable local
@@ -36,6 +43,62 @@ namespace Clase02.Clases
             B = b; //Aquí la propiedad B NO sellama igual que la varible local b, por lo tanto el this es opcional
         }
 
+        /*
+         * Sobrecarga métodos y constructores
+         * La sobrecarga es la creación dentro de la clase, de un grupo de 
+         * métodos que tienen el MISMO NOMBRE pero con un número de parámetros 
+         * DISTINTO y/o bien distintos TIPOS DE DATOS.         
+         */
+        /// <summary>
+        /// Inicializa el valor real e imaginario como cero.
+        /// </summary>
+        public Complejo()
+        {
+            A = 0;
+            B = 0;
+        }
+
+        /// <summary>
+        /// Suma al número complejo de la instancia actual los valores que se le proporcionen
+        /// </summary>
+        /// <param name="Real">Parte real del número complejo</param>
+        /// <param name="Imaginario">Parte imaginaria del número complejo</param>
+        /// <returns>Una instancia con el resultado de la suma del complejo</returns>
+        public Complejo Suma(int Real, int Imaginario)
+        {
+            //Aquí estamos realizano una nueva instancia temporal 
+            // de la clase Complejo
+            var resultado = new Complejo();
+            //Aquí sumamos a la propiedad A de la instancia temporal
+            //El valor de A de la instancia actual + el valor de Real
+            resultado.A = this.A + Real;
+            //Aquí sumamos a la propiedad B de la instancia temporal
+            //El valor de B de la instancia actual + el valor de Imaginario
+            resultado.B = this.B + Imaginario;
+            //Devolvemos la instancia temporal
+            return resultado;
+        }
+
+        //Implementar la resta de numeros complejos
+        //public Complejo Resta(int Real, int Imaginaria)
+
+        /* Suma
+         * z = 5 + 4i
+         * y = 8 + 2i
+         * z + y = -3 + 2i;
+         */
+
+        /* Suma
+         * z = 5 + 4i
+         * y = 8 + 2i
+         * z + y = 13 + 6i;
+         */
+
+        public string FormaBinomica()
+        {
+            return string.Format("{0} + {1}i", A, B);
+            //return A + " + " + B + "i";
+        }
 
     }
 }
--- Clase02/Program.cs
+++ Clase02/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Clase02.Clases;
 
 namespace Clase02
 {
@@ -11,7 +12,19 @@ namespace Clase02
         
         static void Main(string[] args)
         {
-            
+            /* Vamos a realizar una instancia de la clase complejo */
+            var complejo01 = new Complejo(12, 5);
+            var complejo02 = new Complejo();
+
+            Console.WriteLine(complejo01.FormaBinomica());
+            //Aquí estamos declarando que la instancia temporal que devuelve suma
+            //Se guarda en la 'variable' que se llama "res"
+            var res = complejo01.Suma(8, 2);
+
+            Console.WriteLine("Resultado: " + res.FormaBinomica());
+            Console.WriteLine("Complejo01: " + complejo01.FormaBinomica());
+
+            Console.ReadKey();
         }
     }
 }
