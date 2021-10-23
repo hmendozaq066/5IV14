601b7ff8a387316cd1bdc458e4dfe8ba94e582d7
HECTOR MANUEL MENDOZA QUIROZ <hmendoza@ipn.mx>
2021-10-22 19:12:23 -0500

    0712

---
+++ Clase06/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace Clase06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConexion = new System.Windows.Forms.Button();
+            this.txtConsulta = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConexion
+            // 
+            this.btnConexion.Location = new System.Drawing.Point(12, 12);
+            this.btnConexion.Name = "btnConexion";
+            this.btnConexion.Size = new System.Drawing.Size(143, 42);
+            this.btnConexion.TabIndex = 0;
+            this.btnConexion.Text = "Conectar";
+            this.btnConexion.UseVisualStyleBackColor = true;
+            this.btnConexion.Click += new System.EventHandler(this.btnConexion_Click);
+            // 
+            // txtConsulta
+            // 
+            this.txtConsulta.Location = new System.Drawing.Point(161, 12);
+            this.txtConsulta.Multiline = true;
+            this.txtConsulta.Name = "txtConsulta";
+            this.txtConsulta.Size = new System.Drawing.Size(535, 42);
+            this.txtConsulta.TabIndex = 1;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(702, 12);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(143, 42);
+            this.btnConsultar.TabIndex = 2;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.AllowUserToAddRows = false;
+            this.dgvResultados.AllowUserToDeleteRows = false;
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Location = new System.Drawing.Point(12, 60);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.ReadOnly = true;
+            this.dgvResultados.Size = new System.Drawing.Size(927, 442);
+            this.dgvResultados.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(851, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 514);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvResultados);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.txtConsulta);
+            this.Controls.Add(this.btnConexion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConexion;
+        private System.Windows.Forms.TextBox txtConsulta;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.DataGridView dgvResultados;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Clase06/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Clase06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConexion_Click(object sender, EventArgs e)
+        {
+            //1.- Using System.Data.SqlClient;
+            //2.- Cadena de conexión
+            //2.1 Orígenes de datos -> Agregar nuevo origen de datos -> Base de datos -> Siguiente -> conjunto de datos
+            //-> Nueva conexión --> sql server -> nombre del servidor -> autenticación de windows -> selecciona la base de datos
+            //-> Probar conexión -> Avanzadas y copiar la cadena que esta abajo -> cerrar todo
+            //Data Source=DESKTOP-0HLSANU;Initial Catalog=Prueba02;Integrated Security=True
+
+            //Para conectarte necesitas el objeto SqlConnection el cual recibe de parámetro la cadena de conexión
+            var sqlCnn = new SqlConnection("Data Source=DESKTOP-0HLSANU;Initial Catalog=Prueba02;Integrated Security=True");
+            //Abrimos la conexión
+            sqlCnn.Open();
+
+            MessageBox.Show("Conexión exitosa");
+            //Cuando terminamos cerramos la conexión
+            sqlCnn.Close();
+
+
+            //Error 1.- Error relacionado con la red o específico de la instancia mientras se establecía una conexión con el servidor SQL Server. No se encontró el servidor o éste no estaba accesible. Compruebe que el nombre de la instancia es correcto y que SQL Server está configurado para admitir conexiones remotas. (provider: Named Pipes Provider, error: 40 - No se pudo abrir una conexión con SQL Server)
+            //Significa que el nombre del servidor no esta bien escrito (Data source), que no tiene instalado el SQL o el firewall lo esta bloqueando
+            //Solución: Hay que verificar que el nombre del servidor sea el correcto
+
+            //Cannot open database "Prueba03" requested by the login. The login failed. Login failed for user 'SERVIDOR\usuario'
+            //Significa que el nombre de la base datos no esta bien escrita (Initial Catalog) o no existe
+            //Solución: Hay que verificar que el nombre de la base de datos sea la correcta.
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            //PASOS PARA HACER UNA CONSULTA DE TIPO SELECT
+
+            //Para conectarte necesitas el objeto SqlConnection el cual recibe de parámetro la cadena de conexión
+            var sqlCnn = new SqlConnection("Data Source=DESKTOP-0HLSANU;Initial Catalog=Prueba02;Integrated Security=True");
+            //Abrimos la conexión
+            sqlCnn.Open();
+            //Para consultas (SELECT) se usa el objeto SqlDataAdapter 
+            //Nos pide primero la consulta (query) después el objeto sqlConnection
+            var sqlAdapter = new SqlDataAdapter(txtConsulta.Text, sqlCnn);
+            //Hacemos una instancia del objeto DataTable en el cual se almacenarán los resultados
+            var resultados = new DataTable();
+            //El objeto sqlDataAdapter tiene un método que se llama Fill el cual 
+            //llena una tabla (hay pasarla como un argmento)
+            sqlAdapter.Fill(resultados);
+            //DataGridView Tiene la propiedad DataSource que es donde indicamos los datos a mostrar
+            dgvResultados.DataSource = resultados;
+
+            //Cuando terminamos cerramos la conexión
+            sqlCnn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //PASOS PARA HACER UNA CONSULTA DE TIPO SELECT
+
+            //Para conectarte necesitas el objeto SqlConnection el cual recibe de parámetro la cadena de conexión
+            var sqlCnn = new SqlConnection("Data Source=DESKTOP-0HLSANU;Initial Catalog=Prueba02;Integrated Security=True");
+            //Abrimos la conexión
+            sqlCnn.Open();
+            //Para consultas (SELECT) se usa el objeto SqlDataAdapter 
+            //Nos pide primero la consulta (query) después el objeto sqlConnection
+            var sqlAdapter = new SqlDataAdapter("SELECT * FROM usuario", sqlCnn);
+            //Hacemos una instancia del objeto DataTable en el cual se almacenarán los resultados
+            var resultados = new DataTable();
+            //El objeto sqlDataAdapter tiene un método que se llama Fill el cual 
+            //llena una tabla (hay pasarla como un argmento)
+            sqlAdapter.Fill(resultados);
+            //DataGridView Tiene la propiedad DataSource que es donde indicamos los datos a mostrar
+            dgvResultados.DataSource = resultados;
+
+            //Cuando terminamos cerramos la conexión
+            sqlCnn.Close();
+        }
+    }
+}
